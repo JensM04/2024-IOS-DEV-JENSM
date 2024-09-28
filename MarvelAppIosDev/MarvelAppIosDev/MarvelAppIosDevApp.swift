cd9c31724e94b399bf6265ca8fdf7e1dac6090ad
Jens meersschaert <jensm04@MacBook-Air-van-Jens.local>
2024-09-28 12:44:06 +0200

    init

---
+++ MarvelAppIosDev/MarvelAppIosDev/MarvelAppIosDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppIosDevApp.swift
+//  MarvelAppIosDev
+//
+//  Created by Jens meersschaert on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppIosDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarvelAppIosDev/MarvelAppIosDevTests/MarvelAppIosDevTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppIosDevTests.swift
+//  MarvelAppIosDevTests
+//
+//  Created by Jens meersschaert on 28/09/2024.
+//
+
+import Testing
+@testable import MarvelAppIosDev
+
+struct MarvelAppIosDevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
