72fda0f20ba60937bd2a9af31f511f86f5362281
JensM04 <jens.meersschaert@student.hogent.be>
2024-12-20 17:35:16 +0100

    First steps: dependencies, init

--- MarvelAppIosDev/MarvelAppIosDev/MarvelAppIosDevApp.swift
+++ MarvelAppIosDev/MarvelAppIosDev/MarvelAppIosDevApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct MarvelAppIosDevApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
